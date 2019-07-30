4b520279d7d36d6981874e3dc693a9ab11a4294e
Ekaterina <klukvakater@gmail.com>
2019-07-30 16:37:47 +0300

    Добавлен проект

---
+++ gulp-start/README.md
@@ -0,0 +1,14 @@
+#**Структура папок в проекте**
+##app
+###common
+####styles
+####scripts
+####images
+####fonts
+###pages
+####index.pug
+###layouts
+###components
+
+#**Основные команды для запуска проекта**
+##npm start, npm run production
---
+++ gulp-start/webpack.config.js
@@ -0,0 +1,29 @@
+const isProduction = process.env.NODE_ENV === "production";
+
+const config ={
+    mode: isProduction ? "production" : "development",
+entry:{
+    app:"./app/common/scripts/app.js"
+},
+output: {
+    filename:"[name].min.js"
+},
+devtool: isProduction ? false : "source-map",
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"]
+					}
+				}
+			}
+		]
+	}
+
+};
+
+exports.module=config;
