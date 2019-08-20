95776bb68cba1ec85b1c91a998a061b196c81d7f
Ekaterina <klukvakater@gmail.com>
2019-08-20 13:31:27 +0300

    последние изменения

--- gulp-start/gulpfile.js
+++ gulp-start/gulpfile.js
@@ -1,48 +1,55 @@
-//common
+// common
 const gulp = require("gulp");
-const plumber = require("gulp-plumber");//показывает ошибки
-const gulpIf = require("gulp-if");//пакеты для gulp
-//clear task
-const del =require("del");
+const plumber = require("gulp-plumber");
+const gulpIf = require("gulp-if");
 
-//pug task (templates)
-const pug =require("gulp-pug");
-// js (scripts)
-const webpack = require("webpack");
-const gulpWebpack = require("webpack-stream");
-const webpackConfig = require("./webpack.config");
+// clear task
+const del = require("del");
+
+// pug task
+const pug = require("gulp-pug");
+
 // scss task
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const sourcemaps = require("gulp-sourcemaps");
 const minifyCss = require("gulp-clean-css");
+
+//js
+const webpack = require("webpack");
+const gulpWebpack = require("webpack-stream");
+const webpackConfig = require("./webpack.config.js");
+
 //images
 const imagemin = require("gulp-imagemin");
+
 // server
 const browserSync = require("browser-sync").create();
+
+//svg-sprite
+const svgSprite = require("gulp-svg-sprite");
+const svgmin = require("gulp-svgmin");
 
 const isProduction = process.env.NODE_ENV === "production";
 
 const PATHS = {
-app: "./app",
-dist: "./dist"
+	app: "./app",
+	dist: "./dist"
 };
-gulp.task("clear", ()=> {del(PATHS.dist);});//путь к папке,которую хотим удалить
+
+gulp.task("clear", () => del(PATHS.dist));
 
 gulp.task("templates", () => {
-    return gulp
-        .src(`${PATHS.app}/pages/**/*.pug`,
-         { since: gulp.lastRun("templates") })
-        .pipe(plumber())
-        .pipe(pug({ pretty: true }))
-        .pipe(gulp.dest(PATHS.dist));
+	return gulp
+		.src(`${PATHS.app}/pages/**/*.pug`)
+		.pipe(plumber())
+		.pipe(pug({ pretty: true }))
+		.pipe(gulp.dest(PATHS.dist));
 });
 
 gulp.task("styles", () => {
 	return gulp
-		.src(`${PATHS.app}/common/styles/app.scss`, {
-			since: gulp.lastRun("styles")
-		})
+		.src(`${PATHS.app}/common/styles/app.scss`)
 		.pipe(plumber())
 		.pipe(gulpIf(!isProduction, sourcemaps.init()))
 		.pipe(sass())
@@ -52,29 +59,54 @@ gulp.task("styles", () => {
 		.pipe(gulp.dest(`${PATHS.dist}/assets/styles`));
 });
 
-gulp.task("scripts",() =>
-{ 
-    return gulp
-    .src (`${PATHS.app}/common/scripts/*.js`,
-    {since: gulp.lastRun("scripts")})
-.pipe(plumber())
-.pipe(gulpWebpack(webpackConfig,webpack))
-.pipe(gulp.dest('${PATHS.dist}/assets/scripts'));
+gulp.task("scripts", () => {
+	return gulp
+		.src(`${PATHS.app}/common/scripts/*.js`, { since: gulp.lastRun("scripts") })
+		.pipe(plumber())
+		.pipe(gulpWebpack(webpackConfig, webpack))
+		.pipe(gulp.dest(`${PATHS.dist}/assets/scripts`));
 });
 
 gulp.task("images", () => {
 	return gulp
-		.src(`${PATHS.app}/common/images/**/*.+(png|jpg|jpeg|gif|svg|ico)`, {
-			since: gulp.lastRun("images")
-		})
+		.src(`${PATHS.app}/common/images/**/*.+(png|jpg|jpeg|gif|svg|ico)`)
 		.pipe(plumber())
 		.pipe(gulpIf(isProduction, imagemin()))
 		.pipe(gulp.dest(`${PATHS.dist}/assets/images`));
 });
 
 gulp.task("copy", () => {
-    return gulp.src('app/common/fonts/**/*')
-  .pipe(gulp.dest('dist/assets/fonts'))
+	return gulp
+		.src(`${PATHS.app}/common/fonts/**/*`)
+		.pipe(plumber())
+		.pipe(gulp.dest(`${PATHS.dist}/assets/fonts`));
+});
+
+gulp.task("icons", () => {
+	return gulp
+		.src(`${PATHS.app}/common/icons/**/*.svg`)
+		.pipe(plumber())
+		.pipe(svgmin({
+			js2svg: {
+				pretty: true
+			}
+		}))
+		.pipe(
+			svgSprite({
+				mode: {
+					symbol: {
+						sprite: "../dist/assets/images/icons/sprite.svg",
+						render: {
+							scss: {
+								dest:'../app/common/styles/helpers/sprites.scss',
+								template: './app/common/styles/helpers/sprite-template.scss'
+							}
+						}
+					}
+				}
+			})
+		)
+		.pipe(gulp.dest('./'));
 });
 
 gulp.task("server", () => {
@@ -97,7 +129,8 @@ gulp.task("watch", () => {
 gulp.task(
 	"default",
 	gulp.series(
-		gulp.parallel("templates", "styles", "scripts", "images"),
+		"icons",
+		gulp.parallel("templates", "styles", "scripts", "images", "copy"),
 		gulp.parallel("watch", "server")
 	)
 );
@@ -106,7 +139,6 @@ gulp.task(
 	"production",
 	gulp.series(
 		"clear",
-		gulp.parallel("templates", "styles", "scripts", "images","copy")
+		gulp.parallel("templates", "icons", "styles", "scripts", "images",  "copy")
 	)
 );
-
